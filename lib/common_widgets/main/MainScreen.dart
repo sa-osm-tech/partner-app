35b45f1be0eba48265c2d3495ba44347c24d2ec4
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2022-09-14 01:25:08 +0700

    [add] tabs lists in mainscreen

--- lib/common_widgets/main/MainScreen.dart
+++ lib/common_widgets/main/MainScreen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logerex_partner/features/home/screens/HomeScreen.dart';
+import 'package:logerex_partner/features/notification/screens/NotificationScreen.dart';
+import 'package:logerex_partner/features/order_pool/screens/OrderPoolScreen.dart';
+import 'package:logerex_partner/features/orders/screens/OrdersScreen.dart';
+import 'package:logerex_partner/features/profile/screens/ProfileScreen.dart';
 
 class MainScreen extends HookConsumerWidget {
   final int selectedTab = 0;
@@ -8,9 +14,19 @@ class MainScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final tabsList = useMemoized(
+      () => [
+        const HomeScreen(),
+        const NotificationScreen(),
+        const OrderPoolScreen(),
+        const OrdersScreen(),
+        const ProfileScreen(),
+      ],
+    );
     return Scaffold(
-      body: Center(
-        child: Text('Main Screen'),
+      body: IndexedStack(
+        index: 0,
+        children: tabsList,
       ),
       // bottomNavigationBar
     );
