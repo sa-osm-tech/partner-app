1d1f594855198d81cd68369cb645445ae3cc97b9
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2022-09-23 00:43:50 +0700

    [config] colors

---
+++ lib/themes/LGColors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LGColors {
+  LGColors._();
+
+  static const Color primary_100 = Color(0xFFDB5942);
+  static const Color primary_50 = Color(0xFFEDACA0);
+  static const Color secondary_100 = Color(0xFF1C1B1B);
+  static const Color secondary_50 = Color(0xFF8D8D8D);
+  static const Color ternary_100 = Color(0xFFF3F3F3);
+  static const Color gray_70 = Color(0xFF4C4C4C);
+  static const Color gray_50 = Color(0xFF808080);
+  static const Color gray_30 = Color(0xFFB3B3B3);
+  static const Color gray_10 = Color(0xFFE5E5E5);
+  static const Color action_100 = Color(0xFF5B8A9F);
+  static const Color highlight_100 = Color(0xFFED2224);
+  static const Color neutral_100 = Color(0xFFF7C844);
+  static const Color negative_100 = Color(0xFF8F2E1D);
+  static const Color positive_100 = Color(0xFF0E8F58);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color cream = Color(0xFFF2DAC4);
+  static const Color yellow = Color(0xFFEEA058);
+}
