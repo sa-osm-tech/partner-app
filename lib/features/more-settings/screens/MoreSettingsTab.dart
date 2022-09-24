12b6639ff08e1bf7ea46e285821cf70b7bb2d48b
Ratchanon Wattanataweekul <wattanataweekul.r@gmail.com>
2022-09-24 21:10:02 +0700

    [rename] more -> more settings

--- lib/features/more-settings/screens/MoreSettingsTab.dart
+++ lib/features/more-settings/screens/MoreSettingsTab.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MoreTab extends HookConsumerWidget {
-  const MoreTab({Key? key}) : super(key: key);
+class MoreSettingsTab extends HookConsumerWidget {
+  const MoreSettingsTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return const Center(
-      child: Text('More Tab'),
+      child: Text('More Settings Tab'),
     );
   }
 }
